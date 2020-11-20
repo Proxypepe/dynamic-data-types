63cb5795776d5b611764382e27be062c12872a82
Proxypepe <37917466+Proxypepe@users.noreply.github.com>
2020-11-20 04:27:11 +0300

    Update List.h

--- dynamic-data-types/List.h
+++ dynamic-data-types/List.h
@@ -4,43 +4,85 @@
 #include <assert.h>
 
 template <class T>
+class  ListIterator;
+
+template <class T>
+class List;
+// Node class
+template <class T>
+class Tnode
+{
+private:
+	T field = T();
+	Tnode<T>* next = nullptr;
+	friend class List<T>;
+	friend class ListIterator<T>;
+public:
+	Tnode(T data) : field(data) { next = nullptr; };
+	Tnode(){ field = T(); next = nullptr;}
+};
+
+// List Iterator class 
+template <class T>
+class  ListIterator
+{
+private:
+	ListIterator(Tnode<T>* q) : _q(q) {};
+	Tnode<T>* _q;
+	friend class List<T>;
+
+public:
+	bool operator== (ListIterator i) const;
+	bool operator!= (ListIterator i) const;
+	void operator++ ();
+	T& operator*  ();
+};
+
+
+// List class
+template <class T>
 class List
 {
 private:
-	template <class T>
-	class Tnode
-	{
-	public:
-		T field = T();
-		Tnode<T>* next = nullptr;
-	};
 
 	size_t size = 0;
 
 	Tnode<T>* head = nullptr;		
-	Tnode<T>* R = nullptr;
 
 	T get_field();
 
 public:
 	List();
-	~List()
-	{
-		this->clear();
-	}
+	~List()	{this->clear();}
+
 	size_t get_size();
-	int lenght();
-	void print_list();
-	void pop_front();
-	T peek_front();
-	void push_back(T data);
-	void insert(int pos, T data);
-	void del(int pos);
-	bool is_empty();
+
 	void clear();
 
+	void print_list();
+	void print_list_();
 
-	T& operator[](const int i);
+	void push_front(T data);
+	void pop_front();
+
+	void push_back(T data);
+	void pop_back();
+	void insert(size_t pos, T data);
+	void remove(size_t pos);
+
+	void swap(List<T>& other);
+
+	bool is_empty();
+
+
+	T peek_front();
+
+	T& operator[](const size_t i);
+	List<T>& operator=(const List<T>& other);
+
+	ListIterator<T> begin();
+	ListIterator<T> end();
+
 };
 
 template<class T>
@@ -52,154 +94,13 @@ inline T List<T>::get_field()
 template<class T>
 inline List<T>::List()
 {
-	head = nullptr;
+	this->head = nullptr;
 }
 
 template<class T>
 inline size_t List<T>::get_size()
 {
 	return this->size;
-}
-
-
-template<class T>
-inline int List<T>::lenght()
-{
-	Tnode<T>* q = this->head;
-	int _lenght = 0;
-	while (q != nullptr)
-	{
-		_lenght++;
-		q = q->next;
-	}
-	return _lenght;
-}
-
-template<class T>
-inline void List<T>::print_list()
-{
-	//if (!this->is_empty())
-	//{
-		Tnode<T>* q = this->head;
-		while (q != nullptr)
-		{
-			std::cout << q->field << "\n";
-			q = q->next;
-		//}
-	}
-}
-
-template<class T>
-inline void List<T>::pop_front()
-{
-	Tnode<T>* q = this->head;
-	this->head = this->head->next;
-	delete q;
-	this->size--;
-}
-
-template<class T>
-inline T List<T>::peek_front()
-{
-	return this->head->field;
-}
-
-template<class T>
-inline void List<T>::push_back(T data)
-{
-	Tnode<T>* q = new Tnode<T>;																															
-	q->field = data;															
-	q->next = nullptr;															
-	if (this->head == nullptr)															
-	{
-		this->head = q;																
-		this->R = q;
-	}
-	else {
-		this->R->next = q;
-		this->R = q;																
-	}
-	this->size++;
-}
-
-template<class T>
-inline void List<T>::insert(int pos, T data)
-{
-	Tnode<T>* q = this->head;
-	Tnode<T>* tmp = new Tnode<T>;
-	tmp->field = data;
-	if (pos == 0)
-	{
-		tmp->next = this->head;
-		this->head = tmp;
-	}
-	else if (pos == size)
-	{
-		int _i = 0;
-
-		while (_i != pos - 1)
-		{
-			q = q->next;
-			_i++;
-		}
-		this->R->next = tmp;
-		this->R = tmp;
-	}
-	else
-	{
-		int _i = 0;
-
-		while (_i != pos - 1)
-		{
-			q = q->next;
-			_i++;
-		}
-		tmp->next = q->next;
-		q->next = tmp;
-	}
-	this->size++;
-}
-
-template<class T>
-inline void List<T>::del(int pos)
-{
-	Tnode<T>* q = this->head;
-	Tnode<T>* _tmp = this->head;
-	assert(pos >= 0);
-	assert(pos < this->lenght());
-	if (q != nullptr && (pos < this->lenght() - 1) && (pos >= 0))
-	{
-		for (int i = 0; i < pos; i++)
-		{
-			_tmp = q;
-			q = q->next;
-		}
-		if (q == this->head)
-		{
-			this->head = q->next;
-		}
-		else
-		{
-			_tmp->next = _tmp->next->next;
-		}
-		delete q;
-	}
-	else if (q != nullptr &&  (pos == this->lenght()))
-	{
-		this->R->next = q;
-		this->R = q;
-		_tmp->next = q->next;
-		delete q;
-	}
-	this->size--;
-}
-
-template<class T>
-inline bool List<T>::is_empty()
-{
-	if (this->head == nullptr)
-		return true;
-	return false;
 }
 
 template<class T>
@@ -212,9 +113,167 @@ inline void List<T>::clear()
 }
 
 template<class T>
-T& List<T>::operator[](const int i)
+inline void List<T>::print_list()
 {
-	assert(this->head == nullptr);
+	Tnode<T>* q = this->head;
+	while (q != nullptr)
+	{
+		std::cout << q->field << "\n";
+		q = q->next;
+	}
+}
+
+template<class T>
+inline void List<T>::print_list_()
+{
+	Tnode<T>* q = this->head;
+	while (q != nullptr)
+	{
+		q->field.print();
+		std::cout << "\n";
+		q = q->next;
+	}
+}
+
+template<class T>
+inline void List<T>::push_front(T data)
+{
+	Tnode<T>* q = new Tnode<T>;
+	q->field = data;
+	q->next = nullptr;
+	if (this->head == nullptr)
+		this->head = q;
+	else
+	{
+		q->next = this->head;
+		this->head = q;
+	}
+	this->size++;
+}
+
+template<class T>
+inline void List<T>::pop_front()
+{
+	Tnode<T>* q = this->head;
+	this->head = this->head->next;
+	delete q;
+	this->size--;
+}
+
+template<class T>
+inline void List<T>::push_back(T data)
+{
+	Tnode<T>* q; 
+														
+	if (this->head == nullptr)															
+	{
+		q = new Tnode<T>;
+		q->field = data;
+		q->next = nullptr;
+		this->head = q;
+	}
+	else {
+		q = this->head;
+		while (q->next != nullptr)
+			q = q->next;
+		q->next = new Tnode<T>;
+		q->next->field = data;
+		q->next->next = nullptr;
+	}
+	this->size++;
+}
+
+template<class T>
+inline void List<T>::pop_back()
+{
+	if (this->head != nullptr)
+	{
+		Tnode<T>* q = this->head;
+
+		while (q->next->next != nullptr)
+			q = q->next;
+		delete q->next;
+		q->next = nullptr;
+		this->size--;
+	}
+}
+
+template<class T>
+inline void List<T>::insert(size_t pos, T data)
+{
+	if (this->head != nullptr && pos <= this->size)
+	{
+		if (pos == 0)
+			this->push_front(data);
+		else if (pos == this->size)
+			this->push_back(data);
+		else
+		{
+			Tnode<T>* q = this->head;
+			Tnode<T>* _insert = new Tnode<T>(data);
+			size_t iterator = 0;
+			while (iterator != pos - 1)
+			{
+				q = q->next;
+				iterator++;
+			}
+			_insert->next = q->next;
+			q->next = _insert;
+			this->size++;
+		}
+	}
+}
+
+template<class T>
+inline void List<T>::remove(size_t pos)
+{
+	if (this->head != nullptr && pos <= this->size)
+	{
+		if (pos == 0)
+			this->pop_front();
+		else if (pos == this->size)
+			this->pop_back();
+		else
+		{
+			Tnode<T>* q = this->head;
+			Tnode<T>* _delete;
+			for (size_t i = 0; i < pos - 1; i++)
+			{
+				q = q->next;
+			}
+			_delete = q->next;
+			q->next = q->next->next;
+			delete _delete;
+			this->size--;
+		}
+	}
+}
+
+template<class T>
+inline void List<T>::swap(List<T>& other)
+{
+	//pass
+
+
+}
+
+template<class T>
+inline bool List<T>::is_empty()
+{
+	if (this->head == nullptr)
+		return true;
+	return false;
+}
+
+template<class T>
+inline T List<T>::peek_front()
+{
+	return this->head->field;
+}
+
+template<class T>
+T& List<T>::operator[](const size_t i)
+{
 	assert(i < this->size);
 	int counter = 0;
 	Tnode<T>* q = this->head;
@@ -227,3 +286,44 @@ T& List<T>::operator[](const int i)
 	}
 }
 
+template<class T>
+inline List<T>& List<T>::operator=(const List<T>& other)
+{
+	return  *this;
+}
+
+template<class T>
+inline ListIterator<T> List<T>::begin()
+{ 
+	return head;
+}
+
+template<class T>
+inline ListIterator<T> List<T>::end()
+{
+	return nullptr;
+}
+
+template<class T>
+inline bool ListIterator<T>::operator==(ListIterator i) const
+{
+	return this->_q == i._q;
+}
+
+template<class T>
+inline bool ListIterator<T>::operator!=(ListIterator i) const
+{
+	return this->_q != i._q;
+}
+
+template<class T>
+inline void ListIterator<T>::operator++()
+{	
+	this->_q  = this->_q->next;
+}
+
+template<class T>
+inline T& ListIterator<T>::operator*()
+{
+	return this->_q->field;
+}
