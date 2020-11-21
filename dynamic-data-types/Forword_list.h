0391a4d5534cb1f75a6047dbb0ee98b235c45351
Proxypepe <37917466+Proxypepe@users.noreply.github.com>
2020-11-21 13:19:49 +0300

    Update and rename List.h to Forword_list.h

--- dynamic-data-types/Forword_list.h
+++ dynamic-data-types/Forword_list.h
@@ -9,7 +9,7 @@ template <class T>
 class  ListIterator;
 
 template <class T>
-class List;
+class Forword_list;
 // Node class
 template <class T>
 class Tnode
@@ -17,7 +17,7 @@ class Tnode
 private:
 	T field = T();
 	Tnode<T>* next = nullptr;
-	friend class List<T>;
+	friend class Forword_list<T>;
 	friend class ListIterator<T>;
 	Tnode(T data) : field(data) { next = nullptr; };
 	Tnode() { field = T(); next = nullptr; }
@@ -25,14 +25,14 @@ public:
 
 };
 
-// List Iterator class 
+// Forword_list Iterator class 
 template <class T>
 class  ListIterator
 {
 private:
 	ListIterator(Tnode<T>* q) : _q(q) {};
 	Tnode<T>* _q;
-	friend class List<T>;
+	friend class Forword_list<T>;
 
 public:
 	bool operator== (ListIterator i) const;
@@ -42,9 +42,9 @@ public:
 };
 
 
-// List class
+// Forword_list class
 template <class T>
-class List
+class Forword_list
 {
 private:
 
@@ -57,12 +57,12 @@ private:
 	void tree_into_list(BST<T>* v);
 
 public:
-	List();
-	List(size_t count, T value = T());
-	List(const List<T>& x);
-	List(List<T>&& x);
+	Forword_list();
+	Forword_list(size_t count, T value = T());
+	Forword_list(const Forword_list<T>& x);
+	Forword_list(Forword_list<T>&& x);
 
-	~List()	{this->clear();}
+	~Forword_list()	{this->clear();}
 
 	size_t get_size();
 
@@ -80,10 +80,8 @@ public:
 	void insert(size_t pos, T data);
 	void remove(size_t pos);
 
-	void bubble_sort();
-
-	void swap(List<T>& other);
-	void splice(size_t pos, List<T>& other);
+	void swap(Forword_list<T>& other);
+	void splice(size_t pos, Forword_list<T>& other);
 	void reverse();
 	void resize(size_t n, T val = T());
 	void unique();
@@ -91,32 +89,30 @@ public:
 
 	bool is_empty();
 
-
 	T peek_front();
 
 	T& operator[](const size_t i);
-	List<T>& operator=(const List<T>& other);
+	Forword_list<T>& operator=(const Forword_list<T>& other);
 
-	friend std::ostream& operator<<(std::ostream& out, List<T>& list)
+	friend std::ostream& operator<<(std::ostream& out, Forword_list<T>& Forword_list)
 	{
-		for (auto& i : list)
+		for (auto& i : Forword_list)
 			out << " " << i;
 		return out;
 	}
-
 
 	ListIterator<T> begin();
 	ListIterator<T> end();
 };
 
 template<class T>
-inline T List<T>::get_field()
+inline T Forword_list<T>::get_field()
 {
 	return this->field;
 }
 
 template<class T>
-inline void List<T>::fix_size()
+inline void Forword_list<T>::fix_size()
 {
 	if (this->head == nullptr)
 		this->size = 0;
@@ -129,11 +125,12 @@ inline void List<T>::fix_size()
 			this->size++;
 			q = q->next;
 		}
+		this->size--;
 	}
 }
 
 template<class T>
-inline void List<T>::tree_into_list(BST<T>* v)
+inline void Forword_list<T>::tree_into_list(BST<T>* v)
 {
 	if (v == nullptr)
 		return;
@@ -144,19 +141,19 @@ inline void List<T>::tree_into_list(BST<T>* v)
 
 
 template<class T>
-inline List<T>::List()
+inline Forword_list<T>::Forword_list()
 {
 	this->head = nullptr;
 }
 
 template<class T>
-inline List<T>::List(size_t count, T value)
+inline Forword_list<T>::Forword_list(size_t count, T value)
 {
 	this->push_back(value);
 }
 
 template<class T>
-inline List<T>::List(const List<T>& x)
+inline Forword_list<T>::Forword_list(const Forword_list<T>& x)
 {
 	Tnode<T>* q = x.head;
 	while (q != nullptr)
@@ -167,7 +164,7 @@ inline List<T>::List(const List<T>& x)
 }
 
 template<class T>
-inline List<T>::List(List<T>&& x)
+inline Forword_list<T>::Forword_list(Forword_list<T>&& x)
 {
 	Tnode<T>* q = x.head;
 	while (q != nullptr)
@@ -178,21 +175,22 @@ inline List<T>::List(List<T>&& x)
 	x.clear();
 }
 
+
 template<class T>
-inline size_t List<T>::get_size()
+inline size_t Forword_list<T>::get_size()
 {
 	return this->size;
 }
 
 template<class T>
-inline void List<T>::clear()
+inline void Forword_list<T>::clear()
 {
 	while (size)
 		this->pop_front();
 }
 
 template<class T>
-inline void List<T>::print_list()
+inline void Forword_list<T>::print_list()
 {
 	Tnode<T>* q = this->head;
 	while (q != nullptr)
@@ -203,7 +201,7 @@ inline void List<T>::print_list()
 }
 
 template<class T>
-inline void List<T>::print_list_()
+inline void Forword_list<T>::print_list_()
 {
 	Tnode<T>* q = this->head;
 	while (q != nullptr)
@@ -215,7 +213,7 @@ inline void List<T>::print_list_()
 }
 
 template<class T>
-inline void List<T>::push_front(T data)
+inline void Forword_list<T>::push_front(T data)
 {
 	Tnode<T>* q = new Tnode<T>;
 	q->field = data;
@@ -231,7 +229,7 @@ inline void List<T>::push_front(T data)
 }
 
 template<class T>
-inline void List<T>::pop_front()
+inline void Forword_list<T>::pop_front()
 {
 	Tnode<T>* q = this->head;
 	this->head = this->head->next;
@@ -240,7 +238,7 @@ inline void List<T>::pop_front()
 }
 
 template<class T>
-inline void List<T>::push_back(T data)
+inline void Forword_list<T>::push_back(T data)
 {
 	Tnode<T>* q; 
 														
@@ -263,7 +261,7 @@ inline void List<T>::push_back(T data)
 }
 
 template<class T>
-inline void List<T>::pop_back()
+inline void Forword_list<T>::pop_back()
 {
 	if (this->head != nullptr)
 	{
@@ -285,7 +283,7 @@ inline void List<T>::pop_back()
 }
 
 template<class T>
-inline void List<T>::insert(size_t pos, T data)
+inline void Forword_list<T>::insert(size_t pos, T data)
 {
 	try
 	{
@@ -320,7 +318,7 @@ inline void List<T>::insert(size_t pos, T data)
 }
 
 template<class T>
-inline void List<T>::remove(size_t pos)
+inline void Forword_list<T>::remove(size_t pos)
 {
 	if (this->head != nullptr && pos <= this->size)
 	{
@@ -345,13 +343,7 @@ inline void List<T>::remove(size_t pos)
 }
 
 template<class T>
-inline void List<T>::bubble_sort()
-{
-
-}
-
-template<class T>
-inline void List<T>::swap(List<T>& other)
+inline void Forword_list<T>::swap(Forword_list<T>& other)
 {
 	Tnode<T>* q = this->head;
 	Tnode<T>* p = other.head;
@@ -363,7 +355,7 @@ inline void List<T>::swap(List<T>& other)
 }
 
 template<class T>
-inline void List<T>::splice(size_t pos, List<T>& other)
+inline void Forword_list<T>::splice(size_t pos, Forword_list<T>& other)
 {
 	Tnode<T>* q = this->head;
 	size_t  _add_len = this->size - pos;
@@ -384,7 +376,7 @@ inline void List<T>::splice(size_t pos, List<T>& other)
 }
 
 template<class T>
-inline void List<T>::reverse()
+inline void Forword_list<T>::reverse()
 {
 	if (this->head != nullptr && this->size > 1)
 	{
@@ -416,7 +408,7 @@ inline void List<T>::reverse()
 }
 
 template<class T>
-inline void List<T>::resize(size_t n, T val)
+inline void Forword_list<T>::resize(size_t n, T val)
 {
 	if (this->size > n)
 	{
@@ -433,17 +425,42 @@ inline void List<T>::resize(size_t n, T val)
 }
 
 template<class T>
-inline void List<T>::unique()
+inline void Forword_list<T>::unique()
 {
 	if (this->head != nullptr)
 	{
+		size_t _size = this->size;
+		T* uni = new T[_size];
+		for (size_t i = 0; i < _size; i++)
+			uni[i] = T();
 		Tnode<T>* q = this->head;
-		BST<T>* tree = nullptr;
+		size_t _counter = 0;
+		bool flag = true;
+		while (q != nullptr)
+		{
+			for (size_t i = 0; i < _size; i++)
+			{
+				if (q->field == uni[i])
+					flag = false;
+			}
+			if (flag)
+				uni[_counter] = q->field;
+			_counter++;
+			flag = true;
+			q = q->next;
+		}
+		this->clear();
+		for (size_t i = 0; i < _size; i++)
+		{
+			if (uni[i] != T())
+				this->push_back(uni[i]);
+		}
+		delete[] uni;
 	}
 }
 
 template<class T>
-inline void List<T>::sort()
+inline void Forword_list<T>::sort()
 {
 	BST<T>* tree = nullptr;
 	Tnode<T>* q = this->head;
@@ -457,7 +474,7 @@ inline void List<T>::sort()
 }
 
 template<class T>
-inline bool List<T>::is_empty()
+inline bool Forword_list<T>::is_empty()
 {
 	if (this->head == nullptr)
 		return true;
@@ -465,13 +482,13 @@ inline bool List<T>::is_empty()
 }
 
 template<class T>
-inline T List<T>::peek_front()
+inline T Forword_list<T>::peek_front()
 {
 	return this->head->field;
 }
 
 template<class T>
-T& List<T>::operator[](const size_t i)
+T& Forword_list<T>::operator[](const size_t i)
 {
 	assert(i < this->size);
 	int counter = 0;
@@ -486,19 +503,19 @@ T& List<T>::operator[](const size_t i)
 }
 
 template<class T>
-inline List<T>& List<T>::operator=(const List<T>& other)
+inline Forword_list<T>& Forword_list<T>::operator=(const Forword_list<T>& other)
 {
 	return  *this;
 }
 
 template<class T>
-inline ListIterator<T> List<T>::begin()
+inline ListIterator<T> Forword_list<T>::begin()
 { 
 	return head;
 }
 
 template<class T>
-inline ListIterator<T> List<T>::end()
+inline ListIterator<T> Forword_list<T>::end()
 {
 	return nullptr;
 }
